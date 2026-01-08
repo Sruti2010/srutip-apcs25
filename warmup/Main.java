57defdb46b195c79fc80041d17ef46f3137edfab
Sruti2010 <78833981+Sruti2010@users.noreply.github.com>
2026-01-08 23:08:44 +0000

    hi im finished

--- warmup/Main.java
+++ warmup/Main.java
@@ -1,9 +1,16 @@
 package warmup;
 
 public class Main {
-
+int sum;
     public static void main(String[] args) {
-        // Your code here.
-
+        int sum=0;
+        for (int i=0; i<=1000; i++)
+        {
+            if (i%3==0 || i%5==0)
+            {
+                    sum+=i;
+            }
+        }
+        System.out.println("hi this is sum ok:"+sum);
     }
 }
